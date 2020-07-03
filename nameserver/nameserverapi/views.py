b18965e2f880e143486be92dbc91f9e283bc6c68
Ken Smith <k.w.smith@qub.ac.uk>
2020-07-03 17:08:33 +0100

    Added return message instead of sending the object just created back to the user.

--- nameserver/nameserverapi/views.py
+++ nameserver/nameserverapi/views.py
@@ -28,7 +28,8 @@ class EventView(APIView):
     def post(self, request, format=None):
         serializer = EventSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            message = serializer.save()
+            #return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(message, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
