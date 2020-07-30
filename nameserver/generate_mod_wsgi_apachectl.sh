68f44578d4df792861f5d6aaf9f3aa7d9e958599
Ken Smith <k.w.smith@qub.ac.uk>
2020-07-30 22:10:00 +0100

    Replaced /static with /sne/nameserver_atlas/static

--- nameserver/generate_mod_wsgi_apachectl.sh
+++ nameserver/generate_mod_wsgi_apachectl.sh
@@ -2,7 +2,7 @@
 # Create a setup script and immediately start the apache instance.  Our URL prefix
 # is specified by the --mount-point setting.  We need to specify a PYTHONPATH before
 # starting the apache instance. Run this script from THIS directory.
-mod_wsgi-express setup-server --working-directory nameserver --url-alias /static static --application-type module nameserver.wsgi --server-root /tmp --port 8085 --mount-point /sne/nameserver_atlas
+mod_wsgi-express setup-server --working-directory nameserver --url-alias /sne/nameserver_atlas/static static --application-type module nameserver.wsgi --server-root /tmp --port 8085 --mount-point /sne/nameserver_atlas
 
 export PYTHONPATH=$(pwd)
 /tmp/apachectl start
